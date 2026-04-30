669f67ea98ee46def56f15296a309beffa3b2bbc
hwannow <vnfdlvgid061@naver.com>
2026-04-30 14:14:16 +0900

    feat: 진단 반려동물 선택 화면 및 컴포넌트 구현

---
+++ app/src/main/java/com/ppoppi/house/ui/diagnosis/select/PetSelectScreen.kt
@@ -0,0 +1,100 @@
+package com.ppoppi.house.ui.diagnosis.select
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ppoppi.house.R
+import com.ppoppi.house.domain.COLOR
+import com.ppoppi.house.domain.Pet
+import com.ppoppi.house.domain.SEX
+import com.ppoppi.house.domain.SPECIES
+import com.ppoppi.house.ui.component.PetItem
+import com.ppoppi.house.ui.theme.Black
+import com.ppoppi.house.ui.theme.PpoPpiTheme
+import com.ppoppi.house.ui.theme.White
+import com.ppoppi.house.ui.util.getStyledText
+
+@Composable
+fun PetSelectScreen(
+    onBackClick: () -> Unit,
+    onSelect: (pet: Pet) -> Unit,
+    pets: List<Pet>,
+) {
+    Scaffold(
+        topBar = { PetSelectTopAppBar(onBackClick) },
+        containerColor = White,
+    ) { innerPadding ->
+        Column(
+            modifier =
+                Modifier
+                    .padding(innerPadding)
+                    .padding(horizontal = 22.dp),
+        ) {
+            Text(
+                text =
+                    stringResource(R.string.diagnosis_select_hint).getStyledText(
+                        style = PpoPpiTheme.typography.title2,
+                        stringResource(R.string.diagnosis_select_hint_highlight),
+                    ),
+                style = PpoPpiTheme.typography.body2,
+                color = Black,
+                modifier = Modifier.padding(top = 20.dp),
+            )
+
+            LazyColumn(
+                modifier = Modifier.padding(top = 26.dp),
+            ) {
+                items(pets.size) {
+                    PetItem(pet = pets[it], onClick = { onSelect(pets[it]) })
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun PetSelectScreenPreview() {
+    PpoPpiTheme {
+        PetSelectScreen(
+            onBackClick = {},
+            onSelect = {},
+            listOf(
+                Pet(
+                    name = "김은지",
+                    species = SPECIES.DOG,
+                    breed = "사람",
+                    age = 24,
+                    sex = SEX.FEMALE,
+                    color = COLOR.PRIMARY200,
+                ),
+                Pet(
+                    name = "김은지",
+                    species = SPECIES.DOG,
+                    breed = "사람",
+                    age = 24,
+                    sex = SEX.FEMALE,
+                    color = COLOR.PRIMARY50,
+                ),
+                Pet(
+                    name = "김은지",
+                    species = SPECIES.DOG,
+                    breed = "사람",
+                    age = 24,
+                    sex = SEX.FEMALE,
+                    color = COLOR.PRIMARY600,
+                ),
+            ),
+        )
+    }
+}
---
+++ app/src/main/java/com/ppoppi/house/ui/diagnosis/select/SelectActivity.kt
@@ -0,0 +1,65 @@
+package com.ppoppi.house.ui.diagnosis.select
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.ppoppi.house.domain.COLOR
+import com.ppoppi.house.domain.Pet
+import com.ppoppi.house.domain.SEX
+import com.ppoppi.house.domain.SPECIES
+import com.ppoppi.house.ui.diagnosis.camera.CameraActivity
+import com.ppoppi.house.ui.theme.PpoPpiTheme
+
+class SelectActivity : ComponentActivity() {
+    val pets =
+        listOf(
+            Pet(
+                name = "김은지",
+                species = SPECIES.DOG,
+                breed = "사람",
+                age = 24,
+                sex = SEX.FEMALE,
+                color = COLOR.PRIMARY200,
+            ),
+            Pet(
+                name = "김은지",
+                species = SPECIES.DOG,
+                breed = "사람",
+                age = 24,
+                sex = SEX.FEMALE,
+                color = COLOR.PRIMARY50,
+            ),
+            Pet(
+                name = "김은지",
+                species = SPECIES.DOG,
+                breed = "사람",
+                age = 24,
+                sex = SEX.FEMALE,
+                color = COLOR.PRIMARY600,
+            ),
+        )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PpoPpiTheme {
+                PetSelectScreen(
+                    onBackClick = { finish() },
+                    onSelect = { pet ->
+                        val intent = CameraActivity.newIntent(this, pet)
+                        startActivity(intent)
+                    },
+                    pets = pets,
+                )
+            }
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent = Intent(context, SelectActivity::class.java)
+    }
+}
